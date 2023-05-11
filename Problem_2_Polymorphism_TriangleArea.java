7e8ce38649676055e940b8fd46f8b077cdcbaa1f
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-05-12 01:33:30 +0200

    Rectangle & Triangle area by Runtime Polymorphism.

--- Problem_2_Polymorphism_TriangleArea.java
+++ Problem_2_Polymorphism_TriangleArea.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-
 class Shape1{
     double area(){
         return 0;
@@ -19,9 +17,9 @@ class Rectangle extends Shape1{
         return length*width;
     }
 }
-public class Problem_2_TriangleArea extends Shape1{
+public class Problem_2_Polymorphism_TriangleArea extends Shape1{
     double base, height;
-    Problem_2_TriangleArea (double base, double height){
+    Problem_2_Polymorphism_TriangleArea (double base, double height){
         this.base = base;
         this.height = height;
     }
@@ -33,11 +31,14 @@ public class Problem_2_TriangleArea extends Shape1{
     }
 
     public static void main(String[] args) {
+        //Since we are creating many reference variable (s1 , s2, s3) , more than 100 for instance, in super class,
+        //We can use here array. With the help of array, we can create many variables for a same data type.
+
         Shape1 s[] = new Shape1[3];
 
         s[0] = new Shape1();
         s[1] = new Rectangle(10,20);
-        s[2] = new Problem_2_TriangleArea(10,20);
+        s[2] = new Problem_2_Polymorphism_TriangleArea(10,20);
 
         for (int i = 0; i<3; i++){
             System.out.println(s[i].area());
